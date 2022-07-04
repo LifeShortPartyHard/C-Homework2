ff8651f0a23f871340cf26a38780360941e1cc14
Dmitriy <nyx1889gmail.com>
2022-07-04 18:15:41 +0300

    added secondNumber

---
+++ Task10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите трёхзначное число:");
+int number =  Convert.ToInt32(Console.ReadLine());
+if (number < 100 | number > 999) {
+    Console.WriteLine("Данное число не является трёхзначным");
+}
+else {
+    int secondNumber = (number % 100 - number % 10) / 10;
+    Console.Write("Вторая цифра введённого числа: " + secondNumber);
+}
