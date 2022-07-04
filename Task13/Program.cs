c3610e38d9826ede9b09155ae53ddf36904101df
Dmitriy <nyx1889gmail.com>
2022-07-04 19:40:47 +0300

    added thirdNumber

---
+++ Task13/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int [] number2;
+if (number < 100) {
+    Console.WriteLine("В данном числе нет третьей цифры.");
+}
+else {
+    number2 = number.ToString().Select(b => b - '0').ToArray();
+    number = number2[2];
+    Console.WriteLine("Третья цифра числа:" + number);
+}
