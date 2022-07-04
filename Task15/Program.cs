3fa0e67ed58db8c1fbda9419d96a91485ccf9298
Dmitriy <nyx1889gmail.com>
2022-07-04 20:02:35 +0300

    added daysOfWeak

---
+++ Task15/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите число от 1 до 7:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1 | number > 7) {
+    Console.WriteLine("Введённое число не удовлетворяет условию ввода.");
+}
+else {if (number > 0 & number < 6) {
+        Console.WriteLine("День недели не является выходным.");
+    }
+    else {
+        Console.WriteLine("День недели является выходным.");
+    }
+}
